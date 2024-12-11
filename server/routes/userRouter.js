e8fb597b8249c9e8f65b21eb343a69d39559f9f3
Crosshell <confighell@gmail.com>
2024-12-11 02:29:01 +0200

    Add /me endpoint

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,9 +4,13 @@ import {
   getUser,
   updateUser,
 } from '../controllers/userController.js';
+import authenticateToken from '../middlewares/authenticateToken.js';
+import setCurrentUserId from '../middlewares/setCurrentUserId.js';
 
 const userRouter = Router();
 
 userRouter.route('/user/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+userRouter.get('/me', authenticateToken, setCurrentUserId, getUser);
 
 export default userRouter;
