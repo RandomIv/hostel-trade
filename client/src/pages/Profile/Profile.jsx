18b7375ab6f60f41a0e4363c357d105053fdbb2b
kytaec <vladislavtokovenko4@gmail.com>
2024-12-22 11:06:26 +0200

    fixed nav link

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -13,63 +13,67 @@ export default function ProfilePage() {
   console.log(user, hostels);
 
   return (
-    <div className={classes['profile-page']}>
-      <ProfileNavBar></ProfileNavBar>
-      <div className={classes['profile-container']}>
-        <div className={classes['profile-main']}>
-          <div className={classes['profile-image-container']}>
-            <img
-              src={user.avatar_img || emptyPhoto}
-              alt="Your avatar"
-              className={classes['profile-image']}
-            />
+    <>
+      <ProfileNavBar />
+      <div className={classes['profile-page']}>
+        <div className={classes['profile-container']}>
+          <div className={classes['profile-main']}>
+            <div className={classes['profile-image-container']}>
+              <img
+                src={user.avatar_img || emptyPhoto}
+                alt="Your avatar"
+                className={classes['profile-image']}
+              />
+            </div>
+            <ul className={classes['profile-main-description']}>
+              <li>
+                <h2>{user.username}</h2>
+              </li>
+              <li>
+                <b>Пошта:</b>
+                <p>{user.email}</p>
+              </li>
+              <li>
+                <b>Телефон:</b>
+                <p>{user.phone_number}</p>
+              </li>
+            </ul>
+            <div className={classes['profile-main-date']}>
+              <p>З нами з: {user.created_at.split('T')[0]}</p>
+            </div>
           </div>
-          <ul className={classes['profile-main-description']}>
-            <li>
-              <h2>{user.username}</h2>
-            </li>
-            <li>
-              <b>Пошта:</b>
-              <p>{user.email}</p>
-            </li>
-            <li>
-              <b>Телефон:</b>
-              <p>{user.phone_number}</p>
-            </li>
-          </ul>
-          <div className={classes['profile-main-date']}>
-            <p>З нами з: {user.created_at.split('T')[0]}</p>
+          <div className={classes['profile-details']}>
+            <ul>
+              <li>
+                <h2>Особиста інформація</h2>
+              </li>
+              <li>
+                <b>Ім'я:</b>
+                <p>{user.first_name}</p>
+              </li>
+              <li>
+                <b>Прізвище:</b>
+                <p>{user.last_name}</p>
+              </li>
+              <li>
+                <b>Гуртожиток:</b>
+                <p>{user.hostel}</p>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className={classes['profile-details']}>
-          <ul>
-            <li>
-              <h2>Особиста інформація</h2>
-            </li>
-            <li>
-              <b>Ім'я:</b>
-              <p>{user.first_name}</p>
-            </li>
-            <li>
-              <b>Прізвище:</b>
-              <p>{user.last_name}</p>
-            </li>
-            <li>
-              <b>Гуртожиток:</b>
-              <p>{user.hostel}</p>
-            </li>
-          </ul>
+        <div className={classes['profile-manipulation']}>
+          <NavLink
+            to="/profile/profile-settings"
+            className={({ isActive }) =>
+              isActive ? classes['active-link'] : ''
+            }
+          >
+            Редагувати профіль
+          </NavLink>
         </div>
       </div>
-      <div className={classes['profile-manipulation']}>
-        <NavLink
-          to="/profile/profile-settings"
-          className={({ isActive }) => (isActive ? classes['active-link'] : '')}
-        >
-          Редагувати профіль
-        </NavLink>
-      </div>
-    </div>
+    </>
   );
 }
 
--- client/src/pages/ProfileSettings/ProfileSettings.jsx
+++ client/src/pages/ProfileSettings/ProfileSettings.jsx
@@ -57,101 +57,103 @@ export default function ProfileSettingsPage() {
   };
 
   return (
-    <div className={classes['profile-page']}>
+    <>
       <ProfileNavBar />
-      <div className={classes['profile-container']}>
-        <Form
-          method="post"
-          onSubmit={handleSave}
-          className={classes['profile-settings-form']}
-        >
-          <div className={classes['profile-main']}>
-            <div className={classes['profile-image-container']}>
-              <img
-                src={userData.avatar_img || emptyPhoto}
-                alt="Your avatar"
-                className={classes['profile-image']}
-              />
-              <UploadImage handleFileChange={handleFileChange}></UploadImage>
-            </div>
-            <ul className={classes['profile-main-description']}>
-              <li>
-                <DynamicInput
-                  name={'username'}
-                  label={userData.username}
-                  handleInputChange={handleInputChange}
-                >
-                  <h2>{userData.username}</h2>
-                </DynamicInput>
-              </li>
-              <li>
-                <b>Пошта:</b>
-                <DynamicInput
-                  name={'email'}
-                  label={userData.email}
-                  handleInputChange={handleInputChange}
-                >
-                  <p>{userData.email}</p>
-                </DynamicInput>
-              </li>
-              <li>
-                <b>Телефон:</b>
-                <DynamicInput
-                  name={'phone_number'}
-                  label={userData.phone_number}
-                  handleInputChange={handleInputChange}
-                >
-                  <p>{userData.phone_number}</p>
-                </DynamicInput>
-              </li>
-            </ul>
-            <div className={classes['profile-main-date']}>
-              <p>З нами з: {user.created_at.split('T')[0]}</p>
-            </div>
-          </div>
-          <div className={classes['profile-details']}>
-            <ul>
-              <li>
-                <h2>Особиста інформація</h2>
-              </li>
-              <li>
-                <b>Ім'я:</b>
-                <DynamicInput
-                  name={'first_name'}
-                  label={userData.first_name}
-                  handleInputChange={handleInputChange}
-                >
-                  <p>{userData.first_name}</p>
-                </DynamicInput>
-              </li>
-              <li>
-                <b>Прізвище:</b>
-                <DynamicInput
-                  name={'last_name'}
-                  label={userData.last_name}
-                  handleInputChange={handleInputChange}
-                >
-                  <p>{userData.last_name}</p>
-                </DynamicInput>
-              </li>
-              <li>
-                <b>Гуртожиток:</b>
-                <DropdownInput
-                  title="Гуртожиток"
-                  name="hostelId"
-                  data={sortedHostels}
-                  placeholder="Номер"
-                  defaultValue={defaultValue}
-                  type="checkbox"
+      <div className={classes['profile-page']}>
+        <div className={classes['profile-container']}>
+          <Form
+            method="post"
+            onSubmit={handleSave}
+            className={classes['profile-settings-form']}
+          >
+            <div className={classes['profile-main']}>
+              <div className={classes['profile-image-container']}>
+                <img
+                  src={userData.avatar_img || emptyPhoto}
+                  alt="Your avatar"
+                  className={classes['profile-image']}
                 />
-              </li>
-            </ul>
-          </div>
-          <div className={classes['profile-manipulation']}>
-            <button>Зберегти зміни</button>
-          </div>
-        </Form>
+                <UploadImage handleFileChange={handleFileChange}></UploadImage>
+              </div>
+              <ul className={classes['profile-main-description']}>
+                <li>
+                  <DynamicInput
+                    name={'username'}
+                    label={userData.username}
+                    handleInputChange={handleInputChange}
+                  >
+                    <h2>{userData.username}</h2>
+                  </DynamicInput>
+                </li>
+                <li>
+                  <b>Пошта:</b>
+                  <DynamicInput
+                    name={'email'}
+                    label={userData.email}
+                    handleInputChange={handleInputChange}
+                  >
+                    <p>{userData.email}</p>
+                  </DynamicInput>
+                </li>
+                <li>
+                  <b>Телефон:</b>
+                  <DynamicInput
+                    name={'phone_number'}
+                    label={userData.phone_number}
+                    handleInputChange={handleInputChange}
+                  >
+                    <p>{userData.phone_number}</p>
+                  </DynamicInput>
+                </li>
+              </ul>
+              <div className={classes['profile-main-date']}>
+                <p>З нами з: {user.created_at.split('T')[0]}</p>
+              </div>
+            </div>
+            <div className={classes['profile-details']}>
+              <ul>
+                <li>
+                  <h2>Особиста інформація</h2>
+                </li>
+                <li>
+                  <b>Ім'я:</b>
+                  <DynamicInput
+                    name={'first_name'}
+                    label={userData.first_name}
+                    handleInputChange={handleInputChange}
+                  >
+                    <p>{userData.first_name}</p>
+                  </DynamicInput>
+                </li>
+                <li>
+                  <b>Прізвище:</b>
+                  <DynamicInput
+                    name={'last_name'}
+                    label={userData.last_name}
+                    handleInputChange={handleInputChange}
+                  >
+                    <p>{userData.last_name}</p>
+                  </DynamicInput>
+                </li>
+                <li>
+                  <b>Гуртожиток:</b>
+                  <DropdownInput
+                    title="Гуртожиток"
+                    name="hostelId"
+                    data={sortedHostels}
+                    placeholder="Номер"
+                    defaultValue={defaultValue}
+                    type="checkbox"
+                  />
+                </li>
+              </ul>
+            </div>
+            <div className={classes['profile-manipulation']}>
+              <button>Зберегти зміни</button>
+            </div>
+          </Form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
