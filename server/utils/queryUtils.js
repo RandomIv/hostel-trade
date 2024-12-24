7b5887ea8b0e54d9a4ca7a619c403032437d37eb
kytaec <vladislavtokovenko4@gmail.com>
2024-12-24 12:53:42 +0200

    fixed favorites

--- server/utils/queryUtils.js
+++ server/utils/queryUtils.js
@@ -1,23 +1,25 @@
-const applyProductModifiers = (query, filter = {}, sort = {}) => {
+const applyProductModifiers = (query, filter = {}, sort = {}, column = '') => {
+  column += column && '.';
+
   const modifiers = [
     {
       action: 'ilike',
-      column: 'name',
+      column: column + 'name',
       value: filter.name && `%${filter.name}%`,
     },
-    { action: 'gte', column: 'price', value: filter.price?.min },
-    { action: 'lte', column: 'price', value: filter.price?.max },
-    { action: 'eq', column: 'user_id', value: filter.userId },
-    { action: 'in', column: 'type_id', value: filter.typeId },
-    { action: 'in', column: 'hostel.id', value: filter.hostelId },
+    { action: 'gte', column: column + 'price', value: filter.price?.min },
+    { action: 'lte', column: column + 'price', value: filter.price?.max },
+    { action: 'eq', column: column + 'user_id', value: filter.userId },
+    { action: 'in', column: column + 'type_id', value: filter.typeId },
+    { action: 'in', column: column + 'hostel_id', value: filter.hostelId },
     {
       action: 'order',
-      column: 'price',
+      column: column + 'price',
       value: sort.price && { ascending: sort.price === 'asc' },
     },
     {
       action: 'order',
-      column: 'created_at',
+      column: column + 'created_at',
       value: sort.date && { ascending: sort.date === 'asc' },
     },
   ];
