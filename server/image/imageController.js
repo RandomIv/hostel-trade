4989383266e6824714ccf9a87cca0d0fcb6495df
RandomIv <ivanishin.alexander@lll.kpi.ua>
2024-12-25 20:49:49 +0200

    Fix forgot password

--- server/image/imageController.js
+++ server/image/imageController.js
@@ -2,7 +2,6 @@ import Router from 'express';
 import handleAsync from '../utils/handleAsync.js';
 import { sendResponse } from '../utils/responseUtils.js';
 import multer from 'multer';
-import AppError from '../utils/appError.js';
 import { uploadImage } from './imageService.js';
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
@@ -11,11 +10,9 @@ imageController.post(
   '/upload',
   upload.single('image'),
   handleAsync(async (req, res, next) => {
-    console.log(req.body);
     const { id, prevUrl, bucket } = req.body;
     const { file } = req;
     const url = await uploadImage(file, id, prevUrl, bucket, next);
-    console.log(url);
     sendResponse(
       res,
       200,
