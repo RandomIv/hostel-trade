42f1be74adbba9f5979ebd710dc8f1de9246369e
RandomIv <ivanishin.alexander@lll.kpi.ua>
2024-12-25 23:13:16 +0200

    Add image post request

--- server/image/imageController.js
+++ server/image/imageController.js
@@ -3,6 +3,8 @@ import handleAsync from '../utils/handleAsync.js';
 import { sendResponse } from '../utils/responseUtils.js';
 import multer from 'multer';
 import { uploadImage } from './imageService.js';
+import { addImage } from './imageDAL.js';
+import { toSnakeCase } from '../utils/objectUtils.js';
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 const imageController = Router();
@@ -21,6 +23,15 @@ imageController.post(
       },
       'url uploaded successfully',
     );
+  }),
+);
+imageController.post(
+  '/images',
+  handleAsync(async (req, res, next) => {
+    const data = toSnakeCase(req.body);
+    const { error } = await addImage(data);
+    if (error) return next(error);
+    sendResponse(res, 201, null, 'Hostel created successfully');
   }),
 );
 export default imageController;
