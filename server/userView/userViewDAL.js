558f548c2b258d203c4a17f17f279209759c555f
Crosshell <confighell@gmail.com>
2024-12-26 03:33:49 +0200

    Fix add view

--- server/userView/userViewDAL.js
+++ server/userView/userViewDAL.js
@@ -3,5 +3,8 @@ import db from '../config/dbConfig.js';
 export const insertUserView = async (productId, userId) => {
   return db
     .from('user_view')
-    .insert({ product_id: productId, user_id: userId });
+    .upsert(
+      { product_id: productId, user_id: userId },
+      { onConflict: 'product_id, user_id' },
+    );
 };
