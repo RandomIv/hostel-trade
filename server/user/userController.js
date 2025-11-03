d6e3d8b2b8c9654684fd1a864c545c48372a5b31
Alexander Ivanishin <ivanishin.alexander@lll.kpi.ua>
2025-11-03 20:35:55 +0200

    feat(server): add '/me' endpoints

--- server/user/userController.js
+++ server/user/userController.js
@@ -9,38 +9,64 @@ import { deleteUser, fetchUser, updateUser } from './userService.js';
 const userController = Router();
 
 userController.get(
-  '/user/:id',
-  handleAsync(async (req, res, next) => {
-    const { id } = req.params;
+    '/user/:id',
+    handleAsync(async (req, res, next) => {
+        const { id } = req.params;
 
-    const user = await fetchUser(id, next);
+        const user = await fetchUser(id, next);
 
-    sendResponse(res, 200, { user });
-  }),
+        sendResponse(res, 200, { user });
+    }),
 );
 
 userController.patch(
-  '/user/:id',
-  handleAsync(async (req, res, next) => {
-    const { id } = req.params;
-    const dataToUpdate = toSnakeCase(req.body);
+    '/user/:id',
+    handleAsync(async (req, res, next) => {
+        const { id } = req.params;
+        const dataToUpdate = toSnakeCase(req.body);
 
-    await updateUser(id, dataToUpdate, next);
+        await updateUser(id, dataToUpdate, next);
 
-    sendResponse(res, 200, null, 'User updated successfully');
-  }),
+        sendResponse(res, 200, null, 'User updated successfully');
+    }),
 );
 
 userController.delete(
-  '/user/:id',
-  handleAsync(async (req, res, next) => {
-    const { id } = req.params;
+    '/user/:id',
+    handleAsync(async (req, res, next) => {
+        const { id } = req.params;
 
-    await deleteUser(id, next);
+        await deleteUser(id, next);
 
-    sendResponse(res, 200, null, 'User deleted successfully');
-  }),
+        sendResponse(res, 200, null, 'User deleted successfully');
+    }),
 );
 
+userController.get(
+    '/me',
+    authenticateToken,
+    setCurrentUserId,
+    handleAsync(async (req, res, next) => {
+        const { id } = req.params;
+
+        const user = await fetchUser(id, next);
+
+        sendResponse(res, 200, { user });
+    }),
+);
+
+userController.patch(
+    '/me',
+    authenticateToken,
+    setCurrentUserId,
+    handleAsync(async (req, res, next) => {
+        const { id } = req.params;
+        const dataToUpdate = toSnakeCase(req.body);
+
+        await updateUser(id, dataToUpdate, next);
+
+        sendResponse(res, 200, null, 'User updated successfully');
+    }),
+);
 
 export default userController;
