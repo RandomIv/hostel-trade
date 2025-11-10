9c7fd1b201346ac19175c6310af3162f37ae770e
Ederatone <vshkarbanw@ukr.net>
2025-11-10 15:42:29 +0200

    fix(server): correct product controller parsing for undefined query

--- server/product/productController.js
+++ server/product/productController.js
@@ -4,72 +4,72 @@ import { toSnakeCase } from '../utils/objectUtils.js';
 import Router from 'express';
 import authenticateToken from '../middlewares/authenticateToken.js';
 import {
-  addProduct,
-  fetchProduct,
-  fetchProducts,
-  removeProduct,
-  updateProduct,
+    addProduct,
+    fetchProduct,
+    fetchProducts,
+    removeProduct,
+    updateProduct,
 } from './productService.js';
 
 const productController = Router();
 
 productController.get(
-  '/product',
-  handleAsync(async (req, res, next) => {
-    const filter = JSON.parse(req.query?.filter);
-    const sort = JSON.parse(req.query?.sort);
+    '/product',
+    handleAsync(async (req, res, next) => {
+        const filter = JSON.parse(req.query?.filter || '{}');
+        const sort = JSON.parse(req.query?.sort || '{}');
 
-    const products = await fetchProducts(filter, sort, next);
+        const products = await fetchProducts(filter, sort, next);
 
-    sendResponse(res, 200, { products });
-  }),
+        sendResponse(res, 200, { products });
+    }),
 );
 
 productController.get(
-  '/product/:id',
-  handleAsync(async (req, res, next) => {
-    const { id } = req.params;
+    '/product/:id',
+    handleAsync(async (req, res, next) => {
+        const { id } = req.params;
 
-    const product = await fetchProduct(id, next);
+        const product = await fetchProduct(id, next);
 
-    sendResponse(res, 200, { product });
-  }),
+        sendResponse(res, 200, { product });
+    }),
 );
 
 productController.post(
-  '/product',
-  authenticateToken,
-  handleAsync(async (req, res, next) => {
-    const dataToCreate = toSnakeCase(req.body);
-    const userId = req.user.id;
+    '/product',
+    authenticateToken,
+    handleAsync(async (req, res, next) => {
+        const dataToCreate = toSnakeCase(req.body);
+        const userId = req.user.id;
 
-    await addProduct(userId, dataToCreate, next);
+        await addProduct(userId, dataToCreate, next);
 
-    sendResponse(res, 201, null, 'Product created successfully');
-  }),
+        sendResponse(res, 201, null, 'Product created successfully');
+    }),
 );
 
 productController.patch(
-  '/product/:id',
-  handleAsync(async (req, res, next) => {
-    const { id } = req.params;
-    const dataToUpdate = toSnakeCase(req.body);
+    '/product/:id',
+    handleAsync(async (req, res, next) => {
+        const { id } = req.params;
+        const dataToUpdate = toSnakeCase(req.body);
 
-    await updateProduct(id, dataToUpdate, next);
+        await updateProduct(id, dataToUpdate, next);
 
-    sendResponse(res, 200, null, 'Product updated successfully');
-  }),
+        sendResponse(res, 200, null, 'Product updated successfully');
+    }),
 );
 
 productController.delete(
-  '/product/:id',
-  handleAsync(async (req, res, next) => {
-    const { id } = req.params;
+    '/product/:id',
+    handleAsync(async (req, res, next) => {
+        const { id } = req.params;
 
-    await removeProduct(id, next);
+        await removeProduct(id, next);
 
-    sendResponse(res, 200, null, 'Product deleted successfully');
-  }),
+        sendResponse(res, 200, null, 'Product deleted successfully');
+    }),
 );
 
 export default productController;
