3d4c65175e61d1217e34afeb8ad8f1f4c4698f3a
Alexander Ivanishin <ivanishin.alexander@lll.kpi.ua>
2025-11-11 22:47:02 +0200

    fix(client): fix dataConvert function to not be region based

--- client/src/utils/dataConvert.js
+++ client/src/utils/dataConvert.js
@@ -1,4 +1,9 @@
 export function convertDate(data) {
-  const date = new Date(data);
-  return date.toLocaleDateString();
+    const date = new Date(data);
+
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+
+    return `${day}.${month}.${year}`;
 }
